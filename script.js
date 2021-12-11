7e52c826cb049ed71165c426d5079ae3c10db0eb
Nick Melms <nmelms92@gmail.com>
2021-12-10 22:39:42 -0500

    added win logic

--- script.js
+++ script.js
@@ -1,53 +1,104 @@
 let playerTurn = 1;
 let player1Blocks = [];
-let player2Blocks = []
+let player2Blocks = [];
+let player1Color = '';
+let player2Color = '';
+let gameBoard = ['','','','','','','','','']
+let currentPlayer = ''
+let winner = false
 
-const winningCombos =[
-    ['1', '2', '3'],
-    ['1', '4', '7'],
-    ['1', '5', '9'],
-    ['2', '5', '8'],
-    ['3', '6', '9'],
-    ['4', '5', '6'],
-    ['7', '8', '9']
-]
+
+player1Color = prompt('player one choose color');
+player2Color= prompt('player 2 color')
 
 function getId(id){
+
     if (playerTurn == 1){
         if(!player2Blocks.includes(id)){
-            document.getElementById(id).style.backgroundColor = 'red';
+            document.getElementById(id).style.backgroundColor = player1Color;
             playerTurn += 1;
             player1Blocks.push(id)
+            currentPlayer = 'p1'
+            gameBoard[id - 1] = 'p1'
+            console.log(gameBoard)
+            
         }else{
             alert('choose a different box')
         }
         
     }else{
         if(!player1Blocks.includes(id)){
-            document.getElementById(id).style.backgroundColor = 'blue';
+            document.getElementById(id).style.backgroundColor = player2Color;
             playerTurn -= 1;
             player2Blocks.push(id)
+            currentPlayer = 'p2'
+            gameBoard[id - 1] = 'p2'
+            console.log(gameBoard)
         }else{
             alert('choose a different box')
         }
-    }
-    for(let i = 0; i <= winningCombos.length -1; i++){
-        let newArr = []
-        newArr.push(winningCombos[i])
-        for(let k = 0; k <= winningCombos.length -1; k++){
+    };
 
-            let finalArr = []
-            finalArr.push(newArr[k])
-            }
-        }  
-} 
-    
+checkWin();
+if (winner){
+    alert(currentPlayer + ' is the winner')
+    reset()
+}
+}
+
+
+function checkWin(){
+    if (gameBoard[0] === currentPlayer){
+        if(gameBoard[1] === currentPlayer && gameBoard[2] === currentPlayer){
+            winner = true
+        }
+    }
+    if (gameBoard[3] === currentPlayer){
+        if (gameBoard[4] === currentPlayer && gameBoard[5] === currentPlayer){
+            winner = true
+        }
+    }
+    if(gameBoard[6] === currentPlayer){
+        if(gameBoard[7] === currentPlayer && gameBoard[8] === currentPlayer){
+            winner = true
+        }
+    }
+    if(gameBoard[1] === currentPlayer){
+        if(gameBoard[4] === currentPlayer && gameBoard[7] === currentPlayer){
+            winner = true
+        }
+    }
+    if(gameBoard[2] === currentPlayer){
+        if(gameBoard[5] === currentPlayer && gameBoard[8] === currentPlayer){
+            winner = true
+        }
+    }
+    if(gameBoard[0] === currentPlayer){
+        if(gameBoard[3] === currentPlayer && gameBoard[6] === currentPlayer){
+            winner = true
+        }
+    }
+    if(gameBoard[0] === currentPlayer){
+        if(gameBoard[4] === currentPlayer && gameBoard[8] === currentPlayer){
+            winner = true
+        }
+    }
+    if(gameBoard[2] === currentPlayer){
+        if(gameBoard[4] === currentPlayer && gameBoard[6] === currentPlayer){
+            winner = true
+        }
+    }
+}
+
+
 function reset(){
         let boxes = document.querySelectorAll('td')
         player1Blocks = []
         player2Blocks = []
+        winner = false
+        gameBoard = ['','','','','','','','','']
+        playerTurn = 1
         boxes.forEach((e) => {
             e.style.backgroundColor = 'white'
         })
 }
-    
